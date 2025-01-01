f26cd93ed7c7326f4f4429e74e5c17dcd337b333
=Eshetiegashaw <=>
2025-01-01 13:57:45 +0300

    Refactor AppBar and Drawer components for improved readability and updated titles

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pediatric_app/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-
 var my_app_bar = AppBar(
-    backgroundColor: const Color.fromRGBO(0, 183, 227, 1.0),
+  backgroundColor: const Color.fromRGBO(0, 183, 227, 1.0),
   title: const Text('Pediatrics Health'),
   actions: [my_action],
 );
@@ -14,70 +13,71 @@ var my_app_bar = AppBar(
 // rgb(8 104 145)
 // rgb(21 19 58)
 var my_drawer = Drawer(
-    backgroundColor: Colors.white,
-    elevation: 2.0,
-    child: Column(
-      children: [
+  backgroundColor: Colors.white,
+  elevation: 2.0,
+  child: Column(
+    children: [
       Container(
         color: Colors.lightBlue,
         child: DrawerHeader(
-            child: Center(
-              child: Text(
-                  'P E D A T R I C S ',
-                style: TextStyle(fontSize: 35, color: Colors.white),
-              ),
+          child: Center(
+            child: Text(
+              'P E D A T R I C S ',
+              style: TextStyle(fontSize: 35, color: Colors.white),
             ),
-        ),
-      ),
-      ListTile(
-      leading: Icon(Icons.dashboard),
-      title: Text("Tutorial"),
-      ),
-      Divider(),
-      ListTile(
-      leading: Icon(Icons.medical_services),
-      title: Text("Diagnosis"),
-      ),
-      ListTile(
-      leading: Icon(Icons.history),
-      title: Text("History"),
-      ),
-      ListTile(
-      leading: Icon(Icons.analytics),
-      title: Text("Analysis"),
-      ),
-      Spacer(flex: 5,),
-      Divider(),
-      ListTile(
-      leading: Icon(Icons.verified_user),
-      title: Text("Profile"),
-      ),
-      ListTile(
-      leading: Icon(Icons.login),
-      title: Text("Login"),
-      ),
-      ],
-    ),
-);
-
-
-var my_action = PopupMenuButton<String>(
-      onSelected: (String result) {
-        // Handle navigation or other actions here
-      },
-      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-        PopupMenuItem<String>(
-          value: 'Theme',
-          child: IconButton(
-              onPressed: () {
-                Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
-              },
-              icon: const Icon(Icons.light_mode),
           ),
         ),
-        const PopupMenuItem<String>(
-          value: 'Option2',
-          child: Text('Option 2'),
-        ),
-      ],
-    );
+      ),
+      ListTile(
+        leading: Icon(Icons.dashboard),
+        title: Text("Tutorial Updated"),
+      ),
+      Divider(),
+      ListTile(
+        leading: Icon(Icons.medical_services),
+        title: Text("Diagnosis Updated"),
+      ),
+      ListTile(
+        leading: Icon(Icons.history),
+        title: Text("History"),
+      ),
+      ListTile(
+        leading: Icon(Icons.analytics),
+        title: Text("Analysis"),
+      ),
+      Spacer(
+        flex: 5,
+      ),
+      Divider(),
+      ListTile(
+        leading: Icon(Icons.verified_user),
+        title: Text("Profile"),
+      ),
+      ListTile(
+        leading: Icon(Icons.login),
+        title: Text("Login"),
+      ),
+    ],
+  ),
+);
+
+var my_action = PopupMenuButton<String>(
+  onSelected: (String result) {
+    // Handle navigation or other actions here
+  },
+  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+    PopupMenuItem<String>(
+      value: 'Theme',
+      child: IconButton(
+        onPressed: () {
+          Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+        },
+        icon: const Icon(Icons.light_mode),
+      ),
+    ),
+    const PopupMenuItem<String>(
+      value: 'Option2',
+      child: Text('Option 2'),
+    ),
+  ],
+);
